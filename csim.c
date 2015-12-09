8ba88c11125e22b5cd1b271c9cf3d89b9f79288d
sarkisben <bgsarkis@gmail.com>
2015-12-09 13:18:06 -0500

    test commit

--- csim.c
+++ csim.c
@@ -10,29 +10,43 @@
 
 int main(int argc, char **argv)
 {
-    int i, v;
-    v = 0;    
-    for(i = 0; i < argc; i++)
-    {
-        if(strcmp(argv[i], "-h") == 0)
-            printf("Usage is:\n"); 
-
-         if(strcmp(argv[i], "-v") == 0)
-         {
-            v = 1;
-            printf("Verbose %d\n", v);           
-         }
-	 if(strcmp(argv[i], "-s") == 0){
-	   printf("Set index bits: %d ", atoi(argv[i])^2);
-	}
-	if(strcmp(argv[i], "-E") == 0){
-	
-	}
-	if(strcmp(argv[i], "-b") == 0){
-
-	}
-	if(strcmp(argv[i], "-t") == 0){
-
+    int opt, flags;
+    int nsecs, tfnd;
+   
+    tfnd = 0;
+    flags = 0;
+    nsecs = 0;    
+    while((opt = getopt(args, argv, "hvs:E:b:t:") != -1){
+	switch(opt){
+	case 'h':
+		printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+		printf("Options:\n");
+		printf("-h		Print this help message.\n");
+		printf("-v		Optional verbose flag.\n");
+		printf("-s <num>	Number of set index bits.\n");
+		printf("-E <num>	Number of lines per set.\n");
+		printf("-b <num>	Number of block offset bits.\n");
+		printf("-t <file>	Trace file.\n\n");
+		printf("Examples:\n");
+		printf("  linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+		printf("  linux>  ./csim -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+	case 'v':
+	case 's':
+	case 'E':
+	case 'b':
+	case 't':
+	default:
+		printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+		printf("Options:\n");
+		printf("-h		Print this help message.\n");
+		printf("-v		Optional verbose flag.\n");
+		printf("-s <num>	Number of set index bits.\n");
+		printf("-E <num>	Number of lines per set.\n");
+		printf("-b <num>	Number of block offset bits.\n");
+		printf("-t <file>	Trace file.\n\n");
+		printf("Examples:\n");
+		printf("  linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+		printf("  linux>  ./csim -v -s 8 -E 2 -b 4 -t traces/yi.trace\n"); 	
 	}
     }
     printSummary(0, 0, 0);
