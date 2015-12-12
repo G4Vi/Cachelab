89a17d802830b1fac08e1f951e8e218d2aae579f
g4vin <hayesgav@gmail.com>
2015-12-12 16:16:31 -0500

    Actually made a fucking cache

--- csim.c
+++ csim.c
@@ -6,12 +6,33 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+struct line{
+    int valid; 
+    int tag; 
+    int access_time; 
+};
+
+struct actualSet{
+    struct line *listLines;
+};
+
+struct cache{
+        int sets;    
+        int linesSet;         
+        struct actualSet *listSet;
+};
+
+void makeCache(struct cache *aCache, int set, int lines);
+
 int main(int argc, char **argv)
 {
     int opt;
-    int offset, block, set;
+    int lines, block, set;
+
+    struct cache aCache;
+
     char* file = NULL;
-    offset = 0;
+    lines = 0;
     block = 0;
     set = 0;    
     while((opt = getopt(argc, argv, "hs:E:b:t:") != -1)){
@@ -33,10 +54,10 @@ int main(int argc, char **argv)
 		set = atoi(optarg);
 		break;
 	case 'E':
-		block = atoi(optarg);
+		lines = atoi(optarg);
 		break;
 	case 'b':
-		offset = atoi(optarg);
+		block = atoi(optarg);
 		break;
 	case 't':
 		file = optarg;
@@ -56,7 +77,34 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);	
 	}
     }
+
+    makeCache(&aCache, set, lines);
     printSummary(0, 0, 0);
     //printSummary(hit_count, miss_count, eviction_count);
     return 0;
 }
+
+void makeCache(struct cache *aCache, int set, int lines){
+    int i, j;
+	aCache->sets = (2 << set); //number of sets = 2^s
+	aCache->linesSet = lines;
+
+        aCache->listSet = calloc(aCache->sets, sizeof(struct actualSet));
+       
+        if(!aCache->listSet)
+	    exit(-1);        
+
+	for(i = 0; i < aCache->sets; i++)
+        {
+            aCache->listSet[i].listLines = calloc(aCache->linesSet, sizeof(struct line));
+
+            if(!aCache->listSet[i].listLines)
+	        exit(-1);
+
+            for(j = 0; j < aCache->linesSet; j++)
+            {
+                aCache->listSet[i].listLines[j].valid = 0;
+            }
+        }
+}
+
