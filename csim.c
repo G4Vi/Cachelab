c71eb9d197c5f03f29b1cf42d27c110b06b7f273
g4vin <hayesgav@gmail.com>
2015-12-12 14:26:01 -0500

    made it compile

--- csim.c
+++ csim.c
@@ -10,11 +10,11 @@ int main(int argc, char **argv)
 {
     int opt;
     int offset, block, set;
-    char* file = null;
+    char* file = NULL;
     offset = 0;
     block = 0;
     set = 0;    
-    while((opt = getopt(args, argv, "hs:E:b:t:") != -1){
+    while((opt = getopt(argc, argv, "hs:E:b:t:") != -1)){
 	switch(opt){
 	case 'h':
 		printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n");
@@ -56,6 +56,7 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);	
 	}
     }
-    printSummary(hit_count, miss_count, eviction_count);
+    printSummary(0, 0, 0);
+    //printSummary(hit_count, miss_count, eviction_count);
     return 0;
 }
